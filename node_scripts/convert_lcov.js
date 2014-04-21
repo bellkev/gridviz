fd37082b0d117538d07a5c0628a13ef8bbf3c85f
Kevin Bell <kevin.a.bell@gmail.com>
2014-04-20 20:56:37 -0700

    Merging python and js coverage data

---
+++ node_scripts/convert_lcov.js
@@ -0,0 +1,17 @@
+var coveralls = require('coveralls');
+var fs = require('fs');
+
+var inPath = process.argv[2];
+var outPath = process.argv[3];
+
+if (!(inPath && outPath)) {
+    console.log("ERROR: You must specify both an input and output location");
+    console.log("   e.g. node convert_lcov.js in_file out_file");
+}
+
+coveralls.convertLcovToCoveralls(fs.readFileSync(inPath, {encoding: 'utf8'}), {}, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    fs.writeFileSync(outPath, JSON.stringify(data));
+});
---
+++ python_scripts/merge_coveralls.py
@@ -0,0 +1,28 @@
+import json
+import sys
+
+from coveralls.api import Coveralls
+
+
+class MergeCoveralls(Coveralls):
+    def __init__(self, merge_file, **kwargs):
+        super(MergeCoveralls, self).__init__(**kwargs)
+        with open(merge_file, 'rb') as mf:
+            self.external_data = json.loads(mf.read())
+
+    def create_data(self):
+        merge_data = super(MergeCoveralls, self).create_data()
+        merge_data['source_files'] += self.external_data['source_files']
+        return merge_data
+
+try:
+    external_file = sys.argv[1]
+except IndexError:
+    print "ERROR: You must specify an external file to merge"
+    exit(1)
+coveralls = MergeCoveralls(external_file)
+print "Submitting coverage to coveralls.io..."
+result = coveralls.wear()
+print "Coverage submitted!"
+print result['message']
+print result['url']
