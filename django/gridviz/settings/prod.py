e06939c86da47f58e364a86e674d7d6862f420ad
Kevin Bell <kevin.a.bell@gmail.com>
2014-06-01 17:55:53 -0700

    Fixed prod collectstatic

--- django/gridviz/settings/prod.py
+++ django/gridviz/settings/prod.py
@@ -18,6 +18,12 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    join(SYSTEM_ROOT, 'angular'),
+    join(SYSTEM_ROOT, 'compiled_static_dev'),
+    join(SYSTEM_ROOT, 'bower_components'),
+)
+
 SECRET_KEY = environ['DJANGO_SECRET_KEY']
 
 ALLOWED_HOSTS = [host.strip() for host in environ['ALLOWED_HOSTS'].split(',')]
