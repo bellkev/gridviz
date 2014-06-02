fb5b10736aa7101e9c62b89d3ef565c83cd5dbde
Kevin Bell <kevin.a.bell@gmail.com>
2014-06-01 22:24:09 -0700

    Added a simple login test

--- browser_tests/tests.js
+++ browser_tests/tests.js
@@ -5,19 +5,30 @@
 
 // Blow up on errors
 casper.on('resource.received', function (resource) {
-    if (resource.status !== 200) {
-        casper.test.fail('Failed to load resource at ' + resource.url);
+    if (resource.status >= 400) {
+        this.test.fail('Failed to load resource at ' + resource.url);
     }
 });
 casper.on('page.error', function (e) {
-    casper.test.fail(e);
+    this.test.fail(e);
 });
 
 // Test
 casper.test.begin('Run smoke tests on production-style site', function (test) {
-    casper.start(casper.cli.options.start + '/accounts/login/', function () {
+    casper.start(casper.cli.options.start + '/accounts/login/');
+    casper.then(function () {
         test.assertHttpStatus(200, 'make sure login page loads');
-    }).run(function () {
+    });
+    casper.then(function () {
+        this.fill('#login form', {
+            'username': casper.cli.options.testUser,
+            'password': casper.cli.options.testPass
+        }, true);
+    });
+    casper.then(function () {
+        test.assertUrlMatch(/drawings/, 'make sure we can login');
+    });
+    casper.run(function () {
         test.done();
     });
 });
