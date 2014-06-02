4e9396c188a6958aee8f2ee8e0e869e88e8d1cb3
Kevin Bell <kevin.a.bell@gmail.com>
2014-06-01 23:14:46 -0700

    Add a browser test to create a drawing

--- browser_tests/tests.js
+++ browser_tests/tests.js
@@ -13,9 +13,14 @@ casper.on('page.error', function (e) {
     this.test.fail(e);
 });
 
+// Include client scripts
+casper.options.clientScripts.push('bower_components/jquery/dist/jquery.min.js');
+
 // Test
 casper.test.begin('Run smoke tests on production-style site', function (test) {
     casper.start(casper.cli.options.start + '/accounts/login/');
+
+    // Load site
     casper.then(function () {
         test.assertHttpStatus(200, 'make sure login page loads');
     });
@@ -25,9 +30,26 @@ casper.test.begin('Run smoke tests on production-style site', function (test) {
             'password': casper.cli.options.testPass
         }, true);
     });
+
+    // Login
     casper.then(function () {
         test.assertUrlMatch(/drawings/, 'make sure we can login');
     });
+
+    // Make a drawing
+    casper.then(function () {
+        this.click('#new-drawing');
+    });
+    casper.then(function () {
+        this.fill('#drawing-create form', {
+            'title': 'My Drawing'
+        }, true);
+    });
+    casper.then(function () {
+        test.assertSelectorHasText('#drawing-update .content-title', 'My Drawing', 'make sure we can make a drawing');
+    });
+
+    // Edit the drawing
     casper.run(function () {
         test.done();
     });
